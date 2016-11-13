45301677d3dc7459e615e16a59a7498b86fe5bc6
ecarrell <ecarrell@gmail.com>
2016-11-12 23:45:41 -0600

    Basic board layout with minimal functionality (only loads X when a square is clicked).

--- Ultimate-tic-tac-toe/Ultimate-tic-tac-toe/MainPage.xaml.cs
+++ Ultimate-tic-tac-toe/Ultimate-tic-tac-toe/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +27,35 @@ namespace Ultimate_tic_tac_toe
         public MainPage()
         {
             this.InitializeComponent();
+            SetUpBoard();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void SetUpBoard()
+        {
+            foreach (Grid grid in mainGrid.Children)
+            {
+                foreach (Button btn in grid.Children)
+                {
+                    ImageBrush brush1 = new ImageBrush();
+                    brush1.ImageSource = new BitmapImage(new Uri("ms-appx:///images/blank.png"));
+                    btn.Background = brush1;
+                }
+            }
+        }
+
+        private async void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedBtn = sender as Button;
+
+            //var dialog = new MessageDialog("You clicked " + clickedBtn.Name);
+            //await dialog.ShowAsync();
+
+            ImageBrush brush1 = new ImageBrush();
+            brush1.ImageSource = new BitmapImage(new Uri("ms-appx:///images/x.png"));
+            clickedBtn.Background = brush1;
         }
     }
 }
