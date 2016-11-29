581ce674d8b0ae34500cd5ca4cf4af9d785a6c08
benthescott <benjamin.cline@millcreeksoftware.biz>
2016-11-28 22:11:08 -0600

    Beginning Node class

--- Ultimate-tic-tac-toe/Ultimate-tic-tac-toe/Node.cs
+++ Ultimate-tic-tac-toe/Ultimate-tic-tac-toe/Node.cs
@@ -8,5 +8,46 @@ namespace Ultimate_tic_tac_toe
 {
     class Node
     {
+        private Board board;
+        private int alpha;
+        private int beta;
+        private int value;
+
+        public char turn;
+        public int boardSelected;
+
+
+
+        public Node(char t, int selected)
+        {
+            this.turn = t;
+            this.boardSelected = selected;
+        }
+
+        /// <summary>
+        /// 
+        ///     This function statically evaluates the value of a given node in
+        ///     the tree.
+        /// 
+        /// </summary>
+        /// <param name="node">
+        /// 
+        ///     Node to be evaluated.
+        ///     
+        /// </param>
+        /// <returns>
+        /// 
+        ///     Returns the numeric value
+        ///     of the node.
+        /// 
+        /// </returns>
+        private int evaluate(Node node)
+        {
+            int result = 0;
+
+            // TODO make function body
+
+            return result;
+        }
     }
 }
