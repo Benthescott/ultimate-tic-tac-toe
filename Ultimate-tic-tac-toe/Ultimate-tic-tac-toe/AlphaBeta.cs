27ecb71f21b34c40ec95c43d7e9f4333acd6f9bb
benthescott <benjamin.cline@millcreeksoftware.biz>
2016-11-30 01:09:39 -0600

    Re-vamping Node and AlphaBeta classes

--- Ultimate-tic-tac-toe/Ultimate-tic-tac-toe/AlphaBeta.cs
+++ Ultimate-tic-tac-toe/Ultimate-tic-tac-toe/AlphaBeta.cs
@@ -8,41 +8,82 @@ namespace Ultimate_tic_tac_toe
 {
     class AlphaBeta
     {
-
         private bool IsMaxPlayer;
+        private Move SelectedMove;
+        public Board board;
 
         public AlphaBeta()
         {
             IsMaxPlayer = true;
+            board = new Board();
         }
 
-        private Node ConvertToNode(Board b)
-        {
-            Node n = new Node();
 
+        /// <summary>
+        ///     Initializes board to the current state of the game board.
+        /// </summary>
+        /// <param name="b"></param>
+        private void InitBoard(Board b)
+        {
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
                 {
-                    n.board.gameBoard[i, j] = b.gameBoard[i, j];
+                    this.board.boardStatus[i, j] = b.boardStatus[i, j];
+                    this.board.gameBoard[i, j]   = b.gameBoard[i, j];
                 }
             }
-
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    n.board.boardStatus[i, j] = b.boardStatus[i, j];
-                }
-            }
-
-            return n;
         }
 
-        public void MakeMove(Board b, Move s)
+
+        /// <summary>
+        ///     Creates and returns a Node with the coordinates of the most recent move.
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        private Node CreateNode(Move m)
         {
-            int value = AB(ConvertToNode(b), 100, int.MinValue, int.MaxValue, true);
+            return new Node(m);
         }
+
+
+
+
+        /// <summary>
+        ///     This method is the exposed method for the model to receive the 
+        ///     AI's move.
+        /// </summary>
+        /// <param name="b"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public Move MakeAIMove(Board b, Move m)
+        {
+            InitBoard(b);
+            int value = AB(CreateNode(m), 100, int.MinValue, int.MaxValue, true);
+
+            return SelectedMove;
+        }
+
+
+        /// <summary>
+        ///     This function makes a valid move based on the current board.
+        /// </summary>
+        /// <returns></returns>
+        public Move MakeMove()
+        {
+            return new Ultimate_tic_tac_toe.Move();
+        }
+
+
+        /// <summary>
+        ///     This function reverts the most recent move made on the game board.
+        /// </summary>
+        public void UndoMove()
+        {
+
+        }
+
+
 
         /// <summary>
         /// 
@@ -59,14 +100,14 @@ namespace Ultimate_tic_tac_toe
         {
             // If we have drilled down to the max depth, or we found a leaf node,
             // evaluate the node.
-            if (depth == 0 || currentNode.IsTerminalNode(currentNode, depth))
+            if (depth == 0 || IsTerminalNode(depth))
             {
-                return currentNode.evaluate(player);
+                return evaluate(player);
             }
 
             if (player == this.IsMaxPlayer)
             {
-                foreach (Node child in currentNode.CreateSubtree(player))
+                foreach (Node child in Subtree(player))
                 {
                     alpha = Math.Max(alpha, AB(child, depth - 1, alpha, beta, !player));
                     if (beta <= alpha)
@@ -79,7 +120,7 @@ namespace Ultimate_tic_tac_toe
             }
             else
             {
-                foreach (Node child in currentNode.CreateSubtree(player))
+                foreach (Node child in Subtree(player))
                 {
                     beta = Math.Min(beta, AB(child, depth - 1, alpha, beta, !player));
 
@@ -91,6 +132,140 @@ namespace Ultimate_tic_tac_toe
 
                 return beta;
             }
+        }
+
+
+
+
+        /// <summary>
+        /// 
+        ///     Creates subtree to max depth, DFS style
+        /// 
+        /// </summary>
+        /// <param name="Player"></param>
+        /// <returns></returns>
+        public List<Node> Subtree(bool Player)
+        {
+            List<Node> children = new List<Node>();
+
+            /// TODO Create subtree here and return the results
+
+
+
+            return children;
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// 
+        ///     Checks if current node is a leaf node or not. Max depth or game over.
+        /// 
+        /// </summary>
+        /// <param name="Player"></param>
+        /// <returns></returns>
+        public bool IsTerminalNode(int depth)
+        {
+            bool terminalNode = false;
+
+            if (depth >= 100)
+            {
+                terminalNode = true;
+            }
+            else
+            {
+                // First mini game status to check is located at 1,1 in boardStatus array
+                //      The boards are at 1,1; 1,4; 1,7; 4,1; 4,4; 4,7; etc
+                int col = 1;
+                int row = 1;
+
+                // Check for a vertical win
+                for (; col <= 7; col += 3)
+                {
+                    if (board.boardStatus[row, col] == 'X' && board.boardStatus[row + 3, col] == 'X'
+                        && board.boardStatus[row + 6, col] == 'X')
+                        terminalNode = true;
+                    else if (board.boardStatus[row, col] == 'O' && board.boardStatus[row + 3, col] == 'O'
+                        && board.boardStatus[row + 6, col] == 'O')
+                        terminalNode = true;
+                }
+
+                // Reset number
+                col = 1;
+
+                // Check for a horizontal win
+                for (row -= 3; row >= 1; row -= 3)
+                {
+                    if (board.boardStatus[row, col] == 'X' && board.boardStatus[row, col + 3] == 'X'
+                        && board.boardStatus[row, col + 6] == 'X')
+                        terminalNode = true;
+                    else if (board.boardStatus[row, col] == 'O' && board.boardStatus[row, col + 3] == 'O'
+                        && board.boardStatus[row, col + 6] == 'O')
+                        terminalNode = true;
+                }
+
+                // Reset number
+                row = 1;
+
+                // Check for a diagonal win
+                if (board.boardStatus[row, col] == 'X' && board.boardStatus[row + 3, col + 3] == 'X'
+                    && board.boardStatus[row + 6, col + 6] == 'X')
+                    terminalNode = true;
+                else if (board.boardStatus[row, col] == 'O' && board.boardStatus[row + 3, col + 3] == 'O'
+                    && board.boardStatus[row + 6, col + 6] == 'O')
+                    terminalNode = true;
+                else
+                {
+                    if (board.boardStatus[row, col + 6] == 'X' && board.boardStatus[row + 3, col + 3] == 'X'
+                        && board.boardStatus[row + 6, col] == 'X')
+                        terminalNode = true;
+                    else if (board.boardStatus[row, col + 6] == 'O' && board.boardStatus[row + 3, col + 3] == 'O'
+                        && board.boardStatus[row + 6, col] == 'O')
+                        terminalNode = true;
+                }
+
+                // Check TIE condition
+                for (; col <= 7; col += 3)
+                    if (!(board.boardStatus[row, col] == 'B' || board.boardStatus[row + 3, col] == 'B'
+                        || board.boardStatus[row + 6, col] == 'B'))
+                        terminalNode = true;
+            }
+
+            return terminalNode;
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// 
+        ///     This function statically evaluates a given node in
+        ///     the tree. Gives heuristic value of node.
+        /// 
+        /// </summary>
+        /// <param name="player">
+        /// 
+        ///     Boolean to indicate max or min player.
+        ///     
+        /// </param>
+        /// <returns>
+        /// 
+        ///     Returns the heuristic value
+        ///     of the node as an integer.
+        /// 
+        /// </returns>
+        public int evaluate(bool player)
+        {
+            Random rnd = new Random();
+
+            /// TODO write actual heuristic function.
+
+            return rnd.Next(11);
         }
     }
 }
--- Ultimate-tic-tac-toe/Ultimate-tic-tac-toe/Node.cs
+++ Ultimate-tic-tac-toe/Ultimate-tic-tac-toe/Node.cs
@@ -8,132 +8,28 @@ namespace Ultimate_tic_tac_toe
 {
     class Node
     {
-        public Board board;
+        public Move CurrentMove;
+        public int Value;
 
         public Node()
         {
-
+            this.CurrentMove = new Move();
         }
 
-        /// <summary>
-        /// 
-        ///     Creates subtree to max depth, DFS style
-        /// 
-        /// </summary>
-        /// <param name="Player"></param>
-        /// <returns></returns>
-        public List<Node> CreateSubtree(bool Player)
+        public Node(Move m)
         {
-            List<Node> children = new List<Node>();
+            this.CurrentMove = new Move();
 
-            /// TODO Create subtree here and return the results
-
-            return children;
+            this.CurrentMove.row = m.row;
+            this.CurrentMove.col = m.col;
         }
 
-
-        /// <summary>
-        /// 
-        ///     Checks if current node is a leaf node or not. Max depth or game over.
-        /// 
-        /// </summary>
-        /// <param name="Player"></param>
-        /// <returns></returns>
-        public bool IsTerminalNode(Node node, int depth)
+        public Node(int x, int y)
         {
-            bool terminalNode = false;
+            this.CurrentMove = new Move();
 
-            if (depth >= 100)
-            {
-                terminalNode = true;
-            }
-            else
-            {
-                // First mini game status to check is located at 1,1 in boardStatus array
-                //      The boards are at 1,1; 1,4; 1,7; 4,1; 4,4; 4,7; etc
-                int col = 1;
-                int row = 1;
-
-                // Check for a vertical win
-                for (; col <= 7; col += 3)
-                {
-                    if (node.board.boardStatus[row, col] == 'X' && node.board.boardStatus[row + 3, col] == 'X'
-                        && node.board.boardStatus[row + 6, col] == 'X')
-                        terminalNode = true;
-                    else if (node.board.boardStatus[row, col] == 'O' && node.board.boardStatus[row + 3, col] == 'O'
-                        && node.board.boardStatus[row + 6, col] == 'O')
-                        terminalNode = true;
-                }
-
-                // Reset number
-                col = 1;
-
-                // Check for a horizontal win
-                for (row -= 3; row >= 1; row -= 3)
-                {
-                    if (node.board.boardStatus[row, col] == 'X' && node.board.boardStatus[row, col + 3] == 'X'
-                        && node.board.boardStatus[row, col + 6] == 'X')
-                        terminalNode = true;
-                    else if (node.board.boardStatus[row, col] == 'O' && node.board.boardStatus[row, col + 3] == 'O'
-                        && node.board.boardStatus[row, col + 6] == 'O')
-                        terminalNode = true;
-                }
-
-                // Reset number
-                row = 1;
-
-                // Check for a diagonal win
-                if (node.board.boardStatus[row, col] == 'X' && node.board.boardStatus[row + 3, col + 3] == 'X'
-                    && node.board.boardStatus[row + 6, col + 6] == 'X')
-                    terminalNode = true;
-                else if (node.board.boardStatus[row, col] == 'O' && node.board.boardStatus[row + 3, col + 3] == 'O'
-                    && node.board.boardStatus[row + 6, col + 6] == 'O')
-                    terminalNode = true;
-                else
-                {
-                    if (node.board.boardStatus[row, col + 6] == 'X' && node.board.boardStatus[row + 3, col + 3] == 'X'
-                        && node.board.boardStatus[row + 6, col] == 'X')
-                        terminalNode = true;
-                    else if (node.board.boardStatus[row, col + 6] == 'O' && node.board.boardStatus[row + 3, col + 3] == 'O'
-                        && node.board.boardStatus[row + 6, col] == 'O')
-                        terminalNode = true;
-                }
-
-                // Check TIE condition
-                for (; col <= 7; col += 3)
-                    if (!(node.board.boardStatus[row, col] == 'B' || node.board.boardStatus[row + 3, col] == 'B'
-                        || node.board.boardStatus[row + 6, col] == 'B'))
-                        terminalNode = true;
-            }
-            return terminalNode;
-        }
-
-
-
-        /// <summary>
-        /// 
-        ///     This function statically evaluates a given node in
-        ///     the tree. Gives heuristic value of node.
-        /// 
-        /// </summary>
-        /// <param name="player">
-        /// 
-        ///     Boolean to indicate max or min player.
-        ///     
-        /// </param>
-        /// <returns>
-        /// 
-        ///     Returns the heuristic value
-        ///     of the node as an integer.
-        /// 
-        /// </returns>
-        public int evaluate(bool player)
-        {
-            Random rnd = new Random();
-
-            /// TODO make function body
-
-            return rnd.Next(11);
+            this.CurrentMove.row = x;
+            this.CurrentMove.col = y;
         }
     }
 }
